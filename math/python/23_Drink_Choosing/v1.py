8be7c71db7f726dfec9bb7f9139b1b5e0e322443
Aparajita suman <sumankumar86028@gmail.com>
2022-03-28 22:41:39 +0530

    This is the 23rd problem code, but this is not my code, actually I use someone code here.

---
+++ math/python/23_Drink_Choosing/v1.py
@@ -0,0 +1,14 @@
+n, k = map(int, input("Enter: ").split())
+
+C = [0]*(k+1)
+
+for _ in range(n):
+	C[int(input())] += 1
+
+
+odd = 0
+for c in C:
+	if c%2 == 1:
+		odd += 1
+
+print(((n+1)//2)*2- (odd+1)//2)
